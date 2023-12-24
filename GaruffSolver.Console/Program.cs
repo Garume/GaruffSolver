684decf5b3f81dd11bb9cb47c57edb71789f9e0d
ガルム <80187947+desuboruto@users.noreply.github.com>
2023-12-24 21:02:08 +0900

    [add] benchmark to  console application

--- GaruffSolver.Console/Program.cs
+++ GaruffSolver.Console/Program.cs
@@ -1,15 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using GaruffSolver.CNF;
 using GaruffSolver.Solver;
 using GaruffSolver.Solver.DPLL;
 using GaruffSolver.Solver.LiteralSelector;
 using GaruffSolver.Test.Testcases;
 
-var filePath = TestcaseUtility.GetTestcasePath("Sample", "sudoku.cnf");
-var cnf = new CnfReader().ReadFromFile(filePath);
-var builder = new SolveBuilder(new DpllUnitPropagator(), new DpllPureLiteralEliminator(),
-    new MomsLiteralSelector(), new DpllBackTracker());
-var solver = new GaruffSolver.GaruffSolver(builder);
-var model = solver.Solve(cnf);
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<TestSat>();
+    }
+}
+
+public class TestSat
+{
+    private readonly SolveBuilder _builder = new(new DpllUnitPropagator(), new DpllPureLiteralEliminator(),
+        new MomsLiteralSelector(), new DpllBackTracker());
+
+    private readonly CnfReader _reader = new();
+    
+    [Benchmark]
+    public void Test()
+    {
+        var filePath = "C:\\Users\\Garume\\Documents\\Github\\GaruffSolver\\GaruffSolver.Test\\Testcases\\Sample\\sudoku.cnf";
+        var cnf = _reader.ReadFromFile(filePath);
+        var solver = new GaruffSolver.GaruffSolver(_builder);
+        solver.Solve(cnf);
+    }
+}
--- GaruffSolver/Solver/DPLL/DpllPureLiteralEliminator.cs
+++ GaruffSolver/Solver/DPLL/DpllPureLiteralEliminator.cs
@@ -7,28 +7,30 @@ public sealed class DpllPureLiteralEliminator : IPureLiteralEliminator
     public PureLiteralEliminatorFactory PureLiteralElimination =>
         LiteralElimination;
 
+    private Dictionary<ushort, (bool positive, bool negative, Literal literal)> _literalAppearances =
+        new();
+    
     private void LiteralElimination(ref Formula formula, out HashSet<Literal> pureLiterals)
     {
         pureLiterals = new HashSet<Literal>();
 
-        var literalAppearances = new Dictionary<ushort, (bool positive, bool negative, Literal literal)>();
-
+        _literalAppearances.Clear();
         // リテラルの出現を追跡
         foreach (var clause in formula)
         foreach (var literal in clause)
         {
-            if (!literalAppearances.TryGetValue(literal.Value, out var appearance)) appearance = (false, false, literal);
+            if (!_literalAppearances.TryGetValue(literal.Value, out var appearance)) appearance = (false, false, literal);
 
             if (literal.IsPositive)
                 appearance.positive = true;
             else
                 appearance.negative = true;
 
-            literalAppearances[literal.Value] = appearance;
+            _literalAppearances[literal.Value] = appearance;
         }
 
         // 純リテラルを識別し、論理式を単純化
-        foreach (var (_, appearance) in literalAppearances)
+        foreach (var (_, appearance) in _literalAppearances)
             if (appearance.positive != appearance.negative) // 純リテラル
             {
                 pureLiterals.Add(appearance.literal);
